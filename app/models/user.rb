b577d913d82c16ea056be0e8dafe6d47e991f485
Shibbaz <kmosc@protonmail.com>
2022-07-12 12:52:27 +0200

    email validation format user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
   # users.password_hash in the database is a :string
   include BCrypt
 
