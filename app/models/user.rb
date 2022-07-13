55ae6592d4f54bcb7ac17dfdc84615fb00746b32
Shibbaz <107750344+Shibbaz@users.noreply.github.com>
2022-07-13 10:06:15 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
   # users.password_hash in the database is a :string
   include BCrypt
