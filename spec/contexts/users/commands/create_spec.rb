61423396aefb6a93c42f2042451d5c6e0550f9cb
Shibbaz <kmosc@protonmail.com>
2022-07-18 12:12:24 +0200

    refactoring

--- spec/contexts/users/commands/create_spec.rb
+++ spec/contexts/users/commands/create_spec.rb
@@ -3,9 +3,7 @@ require 'faker'
 
 RSpec.describe Contexts::Users::Commands::Create do
   describe '#call' do
-    subject(:command) do
-      described_class.new
-    end
+    subject(:command) { described_class.new }
 
     before { create(:user) }
 
