608c173f781e810153ae0ac798aee3e6f5bd20ef
Shibbaz <107750344+Shibbaz@users.noreply.github.com>
2022-07-19 18:21:15 +0200

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,9 +27,9 @@ class MoviesController < ApplicationController
       @movie = Contexts::Movies::Commands::Create.new.call(params: movie_params)
       format.html { redirect_to movie_url(@movie), notice: 'Movie was successfully created.' }
       format.json { render :show, status: :created, location: @movie }
-    rescue ActiveRecord::RecordInvalid
+    rescue ActiveRecord::RecordInvalid => e
       format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: { message: 'Cannot create the user' }, status: :unprocessable_entity }
+      format.json { render json: { error: e.message }, status: :unprocessable_entity }
     end
   end
 
@@ -39,9 +39,9 @@ class MoviesController < ApplicationController
       @movie = Contexts::Movies::Commands::Update.new.call(movie_params)
       format.html { redirect_to movies_url(@movie), notice: 'Movie was successfully updated.' }
       format.json { render :show, status: :created, location: @movie }
-    rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound => e
       format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: { message: 'Cannot update the user' }, status: :unprocessable_entity }
+      format.json { render json: { error: e.message }, status: :unprocessable_entity }
     end
   end
 
@@ -51,9 +51,9 @@ class MoviesController < ApplicationController
       @movie = Contexts::Movies::Commands::Delete.new.call(params[:id])
       format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
       format.json { head :no_content }
-    rescue ActiveRecord::RecordNotFound
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: { message: 'Cannot delete, such record already does not exist' }, status: :unprocessable_entity }
+    rescue ActiveRecord::RecordNotFound => e
+      format.html { render :destroy, status: :unprocessable_entity }
+      format.json { render json: { error: error.message }, status: :unprocessable_entity }
     end
   end
 
