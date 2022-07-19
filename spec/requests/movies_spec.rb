bf4ebc66b0d1ace90ff4a0c0d0313f7e21be2b10
Shibbaz <107750344+Shibbaz@users.noreply.github.com>
2022-07-19 18:31:10 +0200

    Update movies_spec.rb

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -7,17 +7,15 @@ RSpec.describe 'Movies', type: :request do
       before { create_list(:movie, 10) }
 
       it 'shows 10 movies' do
-        get '/movies',
-            params: {}, as: :json
+        get '/movies', params: {}, as: :json
         expect(JSON(response.body).size > 0).to be(true)
       end
     end
 
     context 'when there is no records' do
       it 'shows 0 movies' do
-        get '/movies',
-            params: {}, as: :json
-        expect(JSON(response.body).size.equal?(0)).to be(true)
+        get '/movies', params: {}, as: :json
+        expect(JSON(response.body).size).to eq(0)
       end
     end
   end
@@ -27,8 +25,7 @@ RSpec.describe 'Movies', type: :request do
       let(:params) { { imdb_id: 1, title: Faker::Name.name, rating: 10.0, year: 1998, rank: 100, data: nil } }
 
       it 'creates a record' do
-        post '/movies',
-             params: params, as: :json
+        post '/movies', params: params, as: :json
         expect(response).to have_http_status(:created)
       end
     end
@@ -36,46 +33,39 @@ RSpec.describe 'Movies', type: :request do
 
   describe 'PUT /movies' do
     context 'when valid params' do
-      before { create(:movie) }
+      let(:movie) { create(:movie) }
 
-      let(:params) { { id: Movie.first.id, imdb_id: 200 } }
-
-      before do
-        put "/movies/#{Movie.first.id}", params:, as: :json
-      end
+      let(:params) { { id: movie.id, imdb_id: 200 } }
 
       it 'updates a record' do
+        put "/movies/#{movie.id}", params:, as: :json
         expect(response).not_to have_http_status(:unprocessable_entity)
       end
     end
 
     context 'when not valid params' do
-      before { create(:movie) }
-
       let(:params) { { id: 1_000_000 } }
 
       it 'does not update a record' do
         put '/movies/1000000', params: params, as: :json
-        expect(JSON(response.body)['message'].empty?).to be(false)
+        expect(JSON(response.body)['error'].empty?).to be(false)
       end
     end
   end
 
   describe 'DELETE /movie' do
     context 'when valid params' do
-      before { create(:movie) }
+      let(:movie) { create(:movie) }
 
-      let(:params) { { id: Movie.first.id } }
+      let(:params) { { id: movie.id } }
 
       it 'deletes a record' do
-        delete "/movies/#{Movie.first.id}", params: params, as: :json
+        delete "/movies/#{movie.id}", params: params, as: :json
         expect(response).not_to have_http_status(:unprocessable_entity)
       end
     end
 
     context 'when not valid params' do
-      before { create(:movie) }
-
       let(:params) { { id: 100_000 } }
 
       it 'does not delete a record' do
