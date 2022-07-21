ded94e3e2446416cdef6041fbeb46bc2ee4c7c98
Shibbaz <kmosc@protonmail.com>
2022-07-21 14:06:44 +0200

    Ip filtering

---
+++ lib/ip_filtering.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class IpFiltering
+  IP_WHITELIST = ['::1'].freeze
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    unless IP_WHITELIST.include? env['action_dispatch.remote_ip'].calculate_ip
+      [200, {}, ['Your IP is not on IP While List!']]
+      return
+    end
+    @app.call(env)
+  end
+end
