613a07db39414ece3bc3dcecd83f68574abbc36f
Shibbaz <kmosc@protonmail.com>
2022-07-21 14:08:32 +0200

    fix

--- lib/ip_filtering.rb
+++ lib/ip_filtering.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class IpFiltering
-  IP_WHITELIST = ['::1'].freeze
+  IP_WHITELIST = ['::1', 'localhost', '127.0.0.1'].freeze
   def initialize(app)
     @app = app
   end
